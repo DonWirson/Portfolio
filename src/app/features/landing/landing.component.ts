f00a60cb1268bfb2765d140ee4648781a18477dc
twilliamson.valdes@gmail.com <twilliamson.valdes@gmail.com>
2025-02-11 19:25:40 -0300

    Agrega botón de demo a proyecto

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,10 +1,10 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,31 +18,43 @@ export class LandingComponent {
       title: "Portafolio",
       svgUrl: "assets/svg/angular-svgrepo-com.svg",
       description: "Literalmente lo estas viendo >:D, creado en Angular 18.",
-      projectUrl: "https://github.com/DonWirson/mvp-git-history-Flutter"
+      projectUrl: "https://github.com/DonWirson/Portfolio",
+      demoUrl: null
     },
     { 
       title: "MVP Historial Git",
       svgUrl: "assets/svg/flutter-svgrepo-com.svg",
       description: "Revisa los commits de un repositorio de Github y sus ramas.",
-      projectUrl: "https://github.com/DonWirson/mvp-git-history-Flutter"
+      projectUrl: "https://github.com/DonWirson/mvp-git-history-Flutter",
+      demoUrl: null
     },
     { 
       title: "Perdidog Api .Net Core",
       svgUrl: "assets/svg/dotnet-svgrepo-com.svg",
       description: "Api utilizada en Perdidog Flutter, actualmente esta en Azure en Prod.",
-      projectUrl: "https://github.com/DonWirson/perdidog-api-net-core"
+      projectUrl: "https://github.com/DonWirson/perdidog-api-net-core",
+      demoUrl: null
     },
     { 
       title: "APP Perdidog",
       svgUrl: "assets/svg/flutter-svgrepo-com.svg",
       description: "App mobile creada en Flutter, con motivo de reportar mascotas perdidas🐕.",
-      projectUrl: "https://github.com/DonWirson/Perdidog-Flutter"
+      projectUrl: "https://github.com/DonWirson/Perdidog-Flutter",
+      demoUrl: null
     },
     { 
       title: "Perdidog Api Spring",
       svgUrl: "assets/svg/java-svgrepo-com.svg",
       description: "Api utilizada en Perdidog Flutter, ahora basada en SpringBoot, de momento no tiene mucha funcionalidad.",
-      projectUrl: "https://github.com/DonWirson/perdidog-api-spring-boot"
+      projectUrl: "https://github.com/DonWirson/perdidog-api-spring-boot",
+      demoUrl: null
+    },
+    { 
+      title: "Mvp Editor Html",
+      svgUrl: "assets/svg/flutter-svgrepo-com.svg",
+      description: "Permite crear archivos HTML usando un editor de texto online o mobile, demo disponible :D.",
+      projectUrl: "https://github.com/DonWirson/mvp-editor-html",
+      demoUrl: "https://donwirson.github.io"
     },
   ]
 }
